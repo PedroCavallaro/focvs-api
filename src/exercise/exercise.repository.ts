a859e6191db8e7e564fa9b7147a7c619622260de
PedroCavallaro <pedrolemecavallaro@gmail.com>
2024-08-13 20:29:21 -0300

    fix: exercise repository

--- src/exercise/exercise.repository.ts
+++ src/exercise/exercise.repository.ts
@@ -21,7 +21,6 @@ export class ExerciseRepository {
 
   async getExercises(muscleId: string) {
     try {
-      console.log(muscleId);
       const exercises = await this.prisma.exercise.findMany({
         where: {
           muscleId,
